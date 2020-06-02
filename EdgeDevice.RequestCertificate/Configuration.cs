778b1627d9bcdc90fcebf0ad55d58c64170c3c54
Roman Marusyk <Marusyk@users.noreply.github.com>
2020-06-03 02:23:53 +0300

    Allow anonymous call to Certificate Authority (#1)

--- EdgeDevice.RequestCertificate/Configuration.cs
+++ EdgeDevice.RequestCertificate/Configuration.cs
@@ -2,8 +2,6 @@
 {
     internal class Configuration
     {
-        public string ClientId { get; set; }
-        public string TenantId { get; set; }
         public string FunctionBaseUrl { get; set; }
         public string DeviceName { get; set; }
     }
